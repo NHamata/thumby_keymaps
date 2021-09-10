b12feca8982df306d2d94f4140d2cb86149e075b
nhamata <nhamata@>
2021-09-10 01:07:03 -0400

    updated config for combo

--- config.h
+++ config.h
@@ -4,5 +4,5 @@
 #define TAPPING_TERM 180 //time between tapdance (ex double tap)
 #define TAPPING_TOGGLE 2 //number of taps to toggle a layer, else holding it activates mo
 #define COMBO_TERM 30 //Default is 50ms
-#define COMBO_COUNT 21
+#define COMBO_COUNT 22
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
